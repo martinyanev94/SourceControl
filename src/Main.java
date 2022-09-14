e841d2aeb6896915b0788c09b26eefa9591e9b13
martinyanev94 <martinyanev94@gmail.com>
2022-09-13 21:21:54 -0400

    Add print statement. Hello GitHub

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int x = 1;
+        System.out.println("Hello GitHub");
+        System.out.println("This is pushed to Git");
     }
 }
